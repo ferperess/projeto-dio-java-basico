79aeaba4995427c9fcb1335023eb4fc01051f36c
ferperess <fepecogo@gmail.com>
2024-08-03 16:45:19 -0300

    Atualização

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,13 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+    
+      //Definição das variáveis
+      int numero;
+      String agencia, nomeCliente;
+      double saldo;
+
+
+
+        System.out.println("Hello, World!");
+    }
+}
