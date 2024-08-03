bcf4fdb4aafa4b6f261ad6bc59600fbfc9457503
ferperess <fepecogo@gmail.com>
2024-08-03 17:08:27 -0300

    Finalização

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+import java.text.DecimalFormat;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
     
@@ -6,8 +8,26 @@ public class ContaTerminal {
       String agencia, nomeCliente;
       double saldo;
 
+     //Coleta dos dados
+     Scanner scanner = new Scanner(System.in);
 
+     System.out.print("Por favor, digite o número da agência: ");
+      agencia = scanner.nextLine();
+     System.out.print("Agora, digite o número da conta: ");
+      numero = scanner.nextInt();
+      scanner.nextLine();
+     System.out.print("Digite seu nome completo: ");
+      nomeCliente = scanner.nextLine();
+     System.out.print("Por último, digite o saldo da conta: ");
+      saldo = scanner.nextDouble();
 
-        System.out.println("Hello, World!");
+     // Formatação do saldo com duas casas decimais
+      DecimalFormat decimalFormat = new DecimalFormat("#.00");
+      String saldoFormatado = decimalFormat.format(saldo);
+      
+     //Mensagem de Cadastro Concluído
+     System.out.println("Cadastro concluído!");
+     System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + 
+     ", conta " + numero + " e seu saldo R$" + saldoFormatado + " já está disponível para saque");
     }
 }
